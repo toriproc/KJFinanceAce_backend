b275ac8d0c1794a662aca42fc79074ee56a244d0
toriproc <hiro.kaji.jan@gmail.com>
2024-05-05 15:03:41 +0900

    postメソッド追加

--- KJFinanceAce_backend/KJFinanceAceApp/views.py
+++ KJFinanceAce_backend/KJFinanceAceApp/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import views
+from rest_framework import views, status
 from .models import InvestmentInfo
 from .serializers import InvestmentInfoSerializer
 
@@ -8,3 +8,10 @@ class InvestmentInfoAPI(views.APIView):
         investment_info = InvestmentInfo.objects.all()
         serializer = InvestmentInfoSerializer(investment_info, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = InvestmentInfoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
