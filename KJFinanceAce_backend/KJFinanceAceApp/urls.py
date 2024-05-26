2252d923c6fafffcaef513ea09edd383a98a5e53
toriproc <hiro.kaji.jan@gmail.com>
2024-05-26 20:06:55 +0900

    delete, putメソッド追加

--- KJFinanceAce_backend/KJFinanceAceApp/urls.py
+++ KJFinanceAce_backend/KJFinanceAceApp/urls.py
@@ -3,5 +3,6 @@ from .views import InvestmentInfoAPI
 
 urlpatterns = [
     path('get-investment-info/', InvestmentInfoAPI.as_view(), name='get-investment-info'),
+    path('get-investment-info/<int:pk>/', InvestmentInfoAPI.as_view(), name='get-investment-info')
     # 他のエンドポイントを追加することもできます
 ]
--- KJFinanceAce_backend/KJFinanceAceApp/views.py
+++ KJFinanceAce_backend/KJFinanceAceApp/views.py
@@ -15,3 +15,26 @@ class InvestmentInfoAPI(views.APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            investment_info_obj = InvestmentInfo.objects.get(pk=pk)
+        except InvestmentInfo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        investment_info_obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        try:
+            investment_info_obj = InvestmentInfo.objects.get(pk=pk)
+        except InvestmentInfo.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        investment_info_obj.key = request.data.get('key', investment_info_obj.key)
+        investment_info_obj.date = request.data.get('date', investment_info_obj.date)
+        investment_info_obj.name = request.data.get('name', investment_info_obj.name)
+        investment_info_obj.totalInvestmentAmount = request.data.get('totalInvestmentAmount', investment_info_obj.totalInvestmentAmount)
+        investment_info_obj.genzaiAmount = request.data.get('genzaiAmount', investment_info_obj.genzaiAmount)
+        # 保存
+        investment_info_obj.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
